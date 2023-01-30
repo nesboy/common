9bfa71aef3193f633dcf3d0ec9f556e1bf7f32ce
Tyrone Cheng <nesboy99@gmail.com>
2023-01-29 19:06:59 -0800

    Integrate with Detekt
    * Remove unnecessary methods from StructuredPlugin interface

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 
 group = "dev.tcheng.common"
 version = "1.0.0"
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("io.gitlab.arturbosch.detekt").version("1.22.0")
     `java-gradle-plugin`
 }
 
@@ -13,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${project.findProperty("detekt.formatting.version")}")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:${project.findProperty("junit.version")}")
 }
@@ -32,4 +36,27 @@ gradlePlugin {
             implementationClass = "dev.tcheng.common.gradle.plugin.EchoPlugin"
         }
     }
+}
+
+detekt {
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        txt.required.set(false)
+        xml.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "11"
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = "11"
 }
--- gradle-plugin/src/main/kotlin/dev/tcheng/common/gradle/plugin/StructuredPlugin.kt
+++ gradle-plugin/src/main/kotlin/dev/tcheng/common/gradle/plugin/StructuredPlugin.kt
@@ -2,7 +2,6 @@ package dev.tcheng.common.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.PluginContainer
 
 abstract class StructuredPlugin<E>(
     private val extensionName: String,
@@ -11,14 +10,11 @@ abstract class StructuredPlugin<E>(
 ) : Plugin<Project> {
 
     override fun apply(project: Project) {
-        this.applyPlugins(project.plugins)
         this.configureTasks(
             project = project,
             extension = project.extensions.create(extensionName, extensionClass)
         )
     }
-
-    abstract fun applyPlugins(pluginContainer: PluginContainer)
 
     abstract fun configureTasks(project: Project, extension: E)
 }
