dc086ee0c5a659b1ce844bd43cf550df6d00e2b9
Tyrone Cheng <nesboy99@gmail.com>
2023-03-03 13:39:55 -0800

    Upgrade dependencies
    * Gradle 8.0.2
    * Log4j 2.20.0
    * Kotlin 1.8.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
