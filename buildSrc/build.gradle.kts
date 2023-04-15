15f9a660375ee55f2571a6a59090461a766141cc
Tyrone Cheng <nesboy99@gmail.com>
2023-04-14 21:14:07 -0700

    Upgrade to Kotlin 1.8.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,10 +7,11 @@ repositories {
 }
 
 dependencies {
+    val dokkaVersion: String by project
     val kotlinVersion: String by project
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
