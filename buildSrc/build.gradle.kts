f8e859666d712a72c769b89adbc13657eed13729
Tyrone Cheng <nesboy99@gmail.com>
2023-09-30 18:07:46 -0700

    Upgrade dependencies
    * Gradle 8.3
    * Kotlin 1.9.10
    * Kover 0.7.3
    * Detekt 1.23.1
    * Dokka 1.9.0
    * MockK 1.13.8
    * JUnit 5.10.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
     val dokkaVersion: String by project
     val kotlinVersion: String by project
 
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.2")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.3")
 }
--- buildSrc/src/main/kotlin/detekt-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/detekt-conventions.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
 }
 
 detekt {
